d0f9123e1eb3396fcc0a051a5c4e613b99bf1246
Artem Stepanenko <98549196+Pipssec@users.noreply.github.com>
2022-06-10 02:40:41 +0300

    Create apteka.py

---
+++ apteka.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://www.ziko.pl/lokalizator/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+adress = soup.find_all('td', class_='mp-table-address')
+time = soup.find_all('td', class_='mp-table-hours')
+name = soup.find_all('span', class_='mp-pharmacy-head')
+# print(adress[0].text)
+class pharmacy:
+    def __init__(self, adres, name, phones, hours):
+        self.address = adres
+        self.name = name
+        self.phones = phones
+        self.working_hours = hours
+
+sort=[]
+for x in range(len(adress)):
+    var1 = adress[x].text
+    index = int(var1.find("tel."))
+    adres = var1[0:int(index)].encode("ascii", errors="replace").decode('utf-8')
+    phone = var1[(index+4):(index+17)]
+    names = name[x].text
+    hours = time[x].text
+    sort.append(pharmacy(adres, names, phone, hours))
+    print(sort[x].__dict__)
+
+jsons = []
+for x in range(len(sort)):
+    apteka = sort[x].__dict__
+    jsons.append(apteka)
+
+with open("apteka_res.json", "w") as write_file:
+    json.dump(jsons, write_file, indent=4)
